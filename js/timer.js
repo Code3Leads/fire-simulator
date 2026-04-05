b0eef8d6c1fc853aee4b44528a185429e305157a
Code3Leads <code3leads@gmail.com>
2026-04-05 16:51:26 -0400

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,20 @@
+let timeLeft = 20;
+let timer;
 
+function startTimer(seconds = 20) {
+  clearInterval(timer);
+
+  timeLeft = seconds;
+  document.getElementById("timer").innerText = "Time: " + timeLeft;
+
+  timer = setInterval(() => {
+    timeLeft--;
+    document.getElementById("timer").innerText = "Time: " + timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      updateScenario("⏰ Time’s up! You hesitated too long.");
+      document.getElementById("choices").innerHTML = "";
+    }
+  }, 1000);
+}
