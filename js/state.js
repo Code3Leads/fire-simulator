44668fd112b8b0b2b170dc88405a2eb1f0745308
Code3Leads <code3leads@gmail.com>
2026-04-05 17:06:43 -0400

    Update state.js

--- js/state.js
+++ js/state.js
@@ -2,5 +2,15 @@ let state = {
   buildingType: "",
   arrival: "",
   fireLocation: "",
+
+  role: "",
+
+  fireIntensity: 5,
+  heatLevel: 5,
+  smokeLevel: 5,
+
+  waterOnFire: false,
+  ventilation: false,
+
   timeElapsed: 0
 };
